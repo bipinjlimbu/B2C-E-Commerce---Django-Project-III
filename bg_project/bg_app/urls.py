02c6318c9f46b60f1f7f5836c2b77bc3f228e343
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-21 08:56:47 +0545

    Register Page Added

--- bg_project/bg_app/urls.py
+++ bg_project/bg_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views.main_view import home_page
+from .views.auth_view import register_view
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('register/', register_view, name='register'),
 ]
