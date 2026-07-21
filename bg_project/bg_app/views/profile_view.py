f4c1513e0f31e4a1c91cd1c8aa4e4bed91255058
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-21 09:12:43 +0545

    Edit Profile Page Added

--- bg_project/bg_app/views/profile_view.py
+++ bg_project/bg_app/views/profile_view.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from ..models import User
 
 @login_required
 def profile_view(request):
     return render(request, 'main/profile_page.html')
+
+@login_required
+def edit_profile_view(request):
+    errors = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        profile_picture = request.FILES.get('profile_picture')
+
+        if not username:
+            errors['username'] = 'Username is required.'
+        elif User.objects.exclude(pk=request.user.pk).filter(username=username).exists():
+            errors['username'] = 'Username is already taken.'
+            
+        if not email:
+            errors['email'] = 'Email is required.'
+        elif User.objects.exclude(pk=request.user.pk).filter(email=email).exists():
+            errors['email'] = 'Email is already taken.'
+
+        if errors:
+            return render(request, 'main/edit_profile_page.html', {'errors': errors, 'data': request.POST})
+
+        user = request.user
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.phone = phone
+        user.address = address
+        if profile_picture:
+            user.profile_picture = profile_picture
+        user.save()
+        
+        messages.success(request, 'Profile updated successfully.')
+        return redirect('/profile/')
+    
+    return render(request, 'main/edit_profile_page.html')
