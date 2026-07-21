09df8d3fc04ceec996ce48a7093d96aa1930968f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-21 09:29:10 +0545

    Brand Management Section Added

--- bg_project/bg_app/views/dashboard.py
+++ bg_project/bg_app/views/dashboard.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from ..models import User
+from ..models import User, Brand
 
 @login_required
 def admin_dashboard_view(request):
@@ -19,7 +19,7 @@ def admin_dashboard_view(request):
         context['customers'] = User.objects.filter(is_staff=False)
         
     if section == 'brand-management':
-        context['brands'] = None
+        context['brands'] = Brand.objects.all().order_by('-created_at')
         
     if section == 'product-management':
         context['products'] = None
