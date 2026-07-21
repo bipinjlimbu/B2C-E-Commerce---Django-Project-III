ee6504f8a26804534ed77fe7287fc0b2bef77fb2
bipinjlimbu <bipinlimbu712@gmail.com>
2026-07-21 10:48:50 +0545

    Products Fetched in Home Page

--- bg_project/bg_app/views/main_view.py
+++ bg_project/bg_app/views/main_view.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from ..models import Product
 
 def home_page(request):
-    return render(request, 'main/home_page.html')
+    products = Product.objects.filter(is_active=True).order_by('-created_at')[:10]
+    return render(request, 'main/home_page.html', {'products': products})
