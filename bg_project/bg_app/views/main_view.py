4753a5ffd680b48d8a1b095735fe63538839490a
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-02 10:27:36 +0545

    Home Page UI Updated

--- bg_project/bg_app/views/main_view.py
+++ bg_project/bg_app/views/main_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from ..models import Product
+from ..models import Product, Brand
 
 def home_page(request):
     products = Product.objects.filter(is_active=True).order_by('-created_at')[:10]
-    return render(request, 'main/home_page.html', {'products': products})
+    brands = Brand.objects.all().order_by('name')
+    return render(request, 'main/home_page.html', {'products': products, 'brands': brands})
